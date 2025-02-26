24418d25191831f37c8b8336e1609d5c90ffbc15
MarkaGK <93790802+MarkaGK@users.noreply.github.com>
2025-02-26 14:39:25 +0300

    Update database.py

--- network_wiki/services/backend/src/database.py
+++ network_wiki/services/backend/src/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 engine = create_async_engine(
-    "postgresql+asyncpg://admin:admin@database_postgres:5432/networks_db", echo=True
+    "postgresql+asyncpg://admin:admin@0.0.0.0:5432/networks_db", echo=True
 )
 async_session_maker = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
