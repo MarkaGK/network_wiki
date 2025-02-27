40a61b59b395d23e27134b026286163584349f48
Filipp <novikovfa16@gmail.com>
2025-02-27 18:00:05 +0000

    Version 1.1.0

--- network_wiki/services/backend/src/tests/test_app.py
+++ network_wiki/services/backend/src/tests/test_app.py
@@ -1,7 +1,6 @@
 """
-Этот модуль для вопроизведения тестов
+Модуль для тестов
 """
-
 
 import pytest
 import pytest_asyncio
@@ -13,17 +12,15 @@ BASE_URL = "http://0.0.0.0:8000"
 async def async_client():
     """
     Создание асинхронного клиента для дальнейшего подключения и проверок
-    :return:
     """
     async with AsyncClient(base_url="http://0.0.0.0:8000") as client:
         yield client
+
 
 @pytest.mark.asyncio
 async def test_ping(async_client):
     """
     Проверка доступности эндпоинта
-    :param async_client
-    :return: response.status_code
     """
     response = await async_client.get(f"{BASE_URL}/uso/uso")
     assert response.status_code == 200
